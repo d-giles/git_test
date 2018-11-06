c4cde69661eb198831793504252ebf1cce0a5b66
Daniel <dgiles1@hawk.iit.edu>
2018-11-06 15:43:20 -0600

    updated to package format

---
+++ build/lib/git_test/code.py
@@ -0,0 +1,6 @@
+
+def do_something():
+    from datetime import datetime
+    something = datetime.now().time()
+    print("The time is {}".format(something))# this will make it much easier in future problems to see that something is actually happening
+    print("Nancy was here.")
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='git_test',
+      version='0.1dev',
+      description='A test of git stuff. Has functionality to tell the time',
+      author='Daniel K Giles',
+      author_email='dgiles1@hawk.iit.edu',
+      packages=['git_test'],
+     )
