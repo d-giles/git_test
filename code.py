b39a873d124a2d4648241ce2f771caabf5da2a42
Daniel <dgiles1@hawk.iit.edu>
2018-11-06 14:31:34 -0600

    Initial commit

---
+++ code.py
@@ -0,0 +1,5 @@
+
+def do_something():
+    from datetime import datetime
+    something = datetime.now().time()
+    print("The time is {}".format(something))# this will make it much easier in future problems to see that something is actually happening
